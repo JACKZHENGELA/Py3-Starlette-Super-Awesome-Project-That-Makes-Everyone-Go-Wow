57dfcf872e9ceeefc25f1c9c93ee561ec143b552
Jack Zheng <260021@bjsmicschool.com>
2023-09-14 10:56:30 +0800

    Added Route about and contact

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ from starlette.routing import Route
 
 import uvicorn
 
+
 async def homepage(request):
     return JSONResponse({"Hello": "World"})
+
+async def about(request):
+    return JSONResponse({"message":"All About Us"})
+
+async def contact(request):
+    return JSONResponse({"message":"E-Mail: 260021@bjsmicschool.com"})
 
 app = Starlette(
     debug=True,
     routes=[
-        Route('/', homepage)
+        Route('/', homepage),
+        Route('/about', about),
+        Route('/contact', contact),
     ]
 )
 
