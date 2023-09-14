434708402b84a384d9661e7d0e9ec1f15e3b85a2
Jack Zheng <260021@bjsmicschool.com>
2023-09-14 10:48:18 +0800

    Imported uvicorn Created homepage in main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route
+
+import uvicorn
+
+async def homepage(request):
+    return JSONResponse({"Hello": "World"})
+
+app = Starlette(
+    debug=True,
+    routes=[
+        Route('/', homepage)
+    ]
+)
+
+uvicorn.run(app, port=8080)
